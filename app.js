ef159edf06de8e1d1aa5458ada96b6474f6ffd3d
yoobin <yoobin.hong@ingka.ikea.com>
2021-07-06 13:31:05 -0400

    initial setup

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+   const squares = document.querySelectorAll('.grid div');
+   const scoreDisplay = document.querySelector('.score span')
+   const startBtn = document.querySelector('.start');
+})
